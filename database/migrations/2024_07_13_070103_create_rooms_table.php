dac8f3b3ad04492474ad05199e8497ebc8fc9b28
kaho-te <kaho.t1031@gmail.com>
2024-07-13 17:34:27 +0900

    マイグレーションファイルの修正

--- database/migrations/2024_07_13_070103_create_rooms_table.php
+++ database/migrations/2024_07_13_070103_create_rooms_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('rooms', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('bucket_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
