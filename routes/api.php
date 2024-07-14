0f73c7a5e73604b8b70acc2d1d8a8741af0dddaa
kaho-te <kaho.t1031@gmail.com>
2024-07-14 18:19:58 +0900

    create route

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/warehouses', [WarehouseController::class, 'index'])->name('warehouses.index');
+Route::post('/warehouses/store', [WarehouseController::class, 'store'])->name('warehouses.store');
+Route::get('/warehouses/download/{warehouse_id}', [WarehouseController::class, 'download'])->name('warehouses.download');
